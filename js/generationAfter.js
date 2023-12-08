d0084e77689c128e14bce083aa377fc492e7e441
Ivan Minaev <minaev70ivan009@gmail.com>
2023-12-09 01:33:39 +0300

    change url

--- js/generationAfter.js
+++ js/generationAfter.js
@@ -3,19 +3,19 @@ import { createGenerateAnswer } from "./answers.js";
 
 export default function generationAfterAnswer() {
     const window = document.querySelector(".window");
-    if (document.location.pathname === "/math-for-kids/add.html") {
+    if (document.location.pathname === "/Math-for-kids/add.html") {
         window.innerHTML = randomExamplePlus("+");
         let generateAnswer = createGenerateAnswer();
         generateAnswer();
-    } else if (document.location.pathname === "/math-for-kids/divide.html") {
+    } else if (document.location.pathname === "/Math-for-kids/divide.html") {
         window.innerHTML = randomExamplePlus("/");
         let generateAnswer = createGenerateAnswer();
         generateAnswer();
-    } else if (document.location.pathname === "/math-for-kids/multiply.html") {
+    } else if (document.location.pathname === "/Math-for-kids/multiply.html") {
         window.innerHTML = randomExamplePlus("X");
         let generateAnswer = createGenerateAnswer();
         generateAnswer();
-    } else if (document.location.pathname === "/math-for-kids/minus.html") {
+    } else if (document.location.pathname === "/Math-for-kids/minus.html") {
         window.innerHTML = randomExamplePlus("-");
         let generateAnswer = createGenerateAnswer();
         generateAnswer();
